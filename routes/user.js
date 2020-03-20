1eb4c042d3b6b14b2d8991e6da0f2a3ec0e3d9eb
ayepez <alex.yepez@outlook.com>
2020-03-19 19:51:54 -0500

    config jwt and middleware

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,49 @@
-const express = require("express");
+const express = require('express')
+const { sign } = require('../libs/auth')
+const authMiddleware = require('../middlewares/auth')
 
-const api = express.Router();
-let users = [{ id: 1, name: "alex" }, { id: 2, name: "xavier" }];
+const api = express.Router()
+let users = [{ id: 1, name: 'alex' }, { id: 2, name: 'xavier' }]
 
-api.get("/list", (req, res) => {
-  res.send(users);
-});
-api.post("/create", (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.send(users);
-});
-api.put("/update/:id", (req, res) => {
-  const { id } = req.params;
-  const newData = req.body;
+api.post('/signin', (req, res) => {
+  const { username, password } = req.body
+  if (username && password) {
+    const result = {
+      username,
+      token: sign({ username, success: true }),
+    }
+    res.send(result)
+  } else {
+    res.status(401).send('username and password required')
+  }
+})
+api.use(authMiddleware)
+api.get('/list', (req, res) => {
+  res.send(users)
+})
+api.post('/create', (req, res) => {
+  const newUser = req.body
+  users.push(newUser)
+  res.send(users)
+})
+api.put('/update/:id', (req, res) => {
+  const { id } = req.params
+  const newData = req.body
   users.map((user, idx) => {
     if (user.id === parseInt(id, 10)) {
-      users[idx] = { ...user, ...newData };
+      users[idx] = { ...user, ...newData }
     }
-  });
-  res.send(users);
-});
-api.delete("/delete", (req, res) => {
-  const { id } = req.query;
+  })
+  res.send(users)
+})
+api.delete('/delete', (req, res) => {
+  const { id } = req.query
   users.map((user, idx) => {
     if (user.id === parseInt(id, 10)) {
-      users.splice(idx, 1);
+      users.splice(idx, 1)
     }
-  });
-  res.send(users);
-});
+  })
+  res.send(users)
+})
 
-module.exports = api;
+module.exports = api
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const http = require("http");
-const express = require("express");
+const http = require('http')
+const express = require('express')
 
-const userRoter = require("./routes/user");
+const authMiddleware = require('./middlewares/auth')
+const userRouter = require('./routes/user')
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
-app.use(express.json());
-app.use("/user", userRoter);
+app.use(express.json())
+app.use('/user', userRouter)
+app.use(authMiddleware)
 
-server.listen("8080", () => {
-  console.log(`aplicacion en ejecucion puesto 8080`);
-});
+server.listen('8080', () => {
+  console.log(`aplicacion en ejecucion puesto 8080`)
+})
